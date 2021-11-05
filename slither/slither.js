0f4c371f5b3a3d4fb452d70527e7e590ea94befc
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-11-05 12:20:22 +0900

    지렁이 게임 (꼬리 구현 완료)

--- slither/slither.js
+++ slither/slither.js
@@ -3,67 +3,16 @@
 var vcanvas, ctx;
 var r_left, r_right, r_up, r_down;
 var feedArr = [];
-var scl = 10;
 var Snake1;
+var scl = 10;
 
-//Snake
 
-var Snake = function() {
-    this.x = 0;
-    this.y = 0;
-    this.xv = 0;
-    this.yv = 0;
-    this.tailArr = [];
-
-    
-    this.updateSnake = function() {
-        var i;
-
-        for ( i = this.tailArr.length - 1; i > 0; i -= 1){
-            this.tailArr[i] = this.tailArr[i-1];
-        }      
-        this.tailArr[0] = { x: this.x, y: this.y };
-
-        this.x += this.xv * scl;
-        this.y += this.yv * scl;
-
-    
-        if( this.x < 0 ) { this.x = 0 }
-        if( this.x + scl > vcanvas.width ) { this.x = vcanvas.width - scl }
-        if( this.y < 0 ) { this.y = 0 }
-        if( this.y + scl > vcanvas.height ) { this.y = vcanvas.height - scl }
-    }
-
-    this.dir = function( x, y ) {
-        this.xv = x;
-        this.yv = y;
-    }
-
-    this.addTail = function( x, y ) {
-        this.tailArr.push({x:x, y:y})
-    }
-
-    this.drawSnake = function() {
-        var i;
-        for( i = 0; i < this.tailArr.length-1; i+=1 ){
-            ctx.beginPath();
-            ctx.fillStyle = "rgb(125,125,125)";
-            ctx.fillRect(this.tailArr[i].x ,this.tailArr[i].y,scl,scl);
-        }
-
-        ctx.fillStyle = 'black';
-        ctx.fillRect(this.x, this.y, scl, scl);
-    }
-
-    
-}
 function init() {
 
     vcanvas = document.getElementById('myCanvas');
     ctx = vcanvas.getContext('2d');
 
     Snake1 = new Snake();
-
 
     setInterval(gameLoop,100);
     setInterval(createFeed,33);
@@ -89,22 +38,70 @@ function drawFeed() {
     var i;
     for(i = 0; i < feedArr.length; i++){
         if(feedArr[i].x % scl == 0 && feedArr[i].y % scl == 0){
+            ctx.beginPath();
             ctx.fillStyle = feedArr[i].c;
             ctx.fillRect(feedArr[i].x, feedArr[i].y, scl, scl);
         }
     }
 }
 
-function eatFeed( snake ) {
+function  eatFeed( snake ) {
     var i;
     for(i = 0; i < feedArr.length; i++){
         if(feedArr[i].x == snake.x && feedArr[i].y == snake.y ){
-            snake.addTail({x:feedArr[i].x, y:feedArr[i].y});
+            snake.addTail();
             feedArr.splice(i,1);
         }
     }
 }
 
+//Snake
+
+var Snake = function() {
+    this.x = 0;
+    this.y = 0;
+    this.xv = 0;
+    this.yv = 0;
+    this.tailArr = [];
+
+    this.updateSnake = function() {
+        
+        var i;
+        for ( i = this.tailArr.length - 1; i > 0; i -= 1) {
+            this.tailArr[i] = this.tailArr[ i-1 ]
+        }
+        this.tailArr[0] = {x: this.x, y: this.y}
+        
+        this.x += this.xv * scl;
+        this.y += this.yv * scl; 
+    
+        if( this.x < 0 ) { this.x = 0 }
+        if( this.x + 10 > vcanvas.width ) { this.x = vcanvas.width - 10 }
+        if( this.y < 0 ) { this.y = 0 }
+        if( this.y + 10 > vcanvas.height ) { this.y = vcanvas.height - 10 }
+    }
+
+    this.dir = function( x,y ) {
+        this.xv = x;
+        this.yv = y;
+    }
+
+    this.addTail = function() {
+        this.tailArr.push({x:null,y:null})
+    }
+
+    this.drawSnake = function() {
+        var i;
+        for ( i = 0; i < this.tailArr.length-1; i++ ){
+            ctx.beginPath();
+            ctx.fillStyle = "gray";
+            ctx.fillRect(this.tailArr[i].x, this.tailArr[i].y, scl, scl);
+        }
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.x, this.y, scl, scl);
+    }
+}
 
 
 function clearCanvas() {
@@ -112,32 +109,34 @@ function clearCanvas() {
 }
 
 function update() {
-    if( r_left ) { Snake1.dir (-1, 0)}
-    if( r_right ) { Snake1.dir (1, 0)}
-    if( r_up ) { Snake1.dir (0, -1)}
-    if( r_down ) { Snake1.dir (0, 1)}
+    if(r_left) { Snake1.dir(-1,0)}
+    if(r_right) { Snake1.dir(1,0)}
+    if(r_up) { Snake1.dir(0,-1)}
+    if(r_down) { Snake1.dir(0,1)}
 }
 
 function gameLoop() {
     clearCanvas();
+    
+    Snake1.drawSnake();
     update();
     Snake1.updateSnake();
 
     drawFeed();
-    eatFeed(Snake1);
-    Snake1.drawSnake();
+    eatFeed( Snake1 );
+
 }
 
 
 
 
 function set_key(event) {
-    r_left = 0; r_up = 0; r_down =0; r_right = 0;
+    r_left = r_up = r_down = r_right = 0;
 
-    if(event.keyCode === 37 ) { r_left = 1; }
-    if(event.keyCode === 38 ) { r_up = 1; }
-    if(event.keyCode === 39) {  r_right= 1; }
-    if(event.keyCode === 40) { r_down = 1; }
+    if(event.keyCode === 37 ) {  r_left = 1 }
+    if(event.keyCode === 38 ) { r_up = 1 }
+    if(event.keyCode === 39) {  r_right= 1 }
+    if(event.keyCode === 40) { r_down = 1 }
 }
 
 document.onkeydown = set_key;
